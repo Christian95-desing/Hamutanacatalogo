2b502d937a067919091b16df8c15c7af582aeb1b
Christian95-desing <crisalexander151@gmail.com>
2024-12-13 21:01:42 -0500

    Imagenes Navida

--- src/media/infraestructure/provider/nuevos.ts
+++ src/media/infraestructure/provider/nuevos.ts
@@ -1,5 +1,17 @@
 // En tu archivo barrel.js
 import { type GaleryImg } from '~/media/domain/GaleryImg.interface';
+import  Image_nuevos_10  from '~/media/hamutana-galery/Nuevos/Navidad1.jpg?jsx';
+import  Image_nuevos_11  from '~/media/hamutana-galery/Nuevos/Navidad2.jpg?jsx';
+import  Image_nuevos_12  from '~/media/hamutana-galery/Nuevos/Navidad3.jpg?jsx';
+import  Image_nuevos_13  from '~/media/hamutana-galery/Nuevos/Navidad4.jpg?jsx';
+import  Image_nuevos_14  from '~/media/hamutana-galery/Nuevos/Navidad5.jpg?jsx';
+import  Image_nuevos_15  from '~/media/hamutana-galery/Nuevos/Navidad6.jpg?jsx';
+import  Image_nuevos_16  from '~/media/hamutana-galery/Nuevos/Navidad7.jpg?jsx';
+import  Image_nuevos_17  from '~/media/hamutana-galery/Nuevos/Navidad8.jpg?jsx';
+import  Image_nuevos_18  from '~/media/hamutana-galery/Nuevos/Navidad9.jpg?jsx';
+import  Image_nuevos_19  from '~/media/hamutana-galery/Nuevos/Navidad10.jpg?jsx';
+import  Image_nuevos_20  from '~/media/hamutana-galery/Nuevos/Navidad11.jpg?jsx';
+import  Image_nuevos_21  from '~/media/hamutana-galery/Nuevos/Navidad12.jpg?jsx';
 import  Image_nuevos_1  from '~/media/hamutana-galery/Nuevos/MAQUEEN_BUZO.jpg?jsx';
 import  Image_nuevos_2  from '~/media/hamutana-galery/Nuevos/MAQUEEN_CAMISETA.jpg?jsx';
 import  Image_nuevos_3  from '~/media/hamutana-galery/Nuevos/MAQUEEN_pareja.jpg?jsx';
@@ -13,6 +25,18 @@ import  Image_nuevos_9  from '~/media/hamutana-galery/Nuevos/tradicional2.jpg?js
 
 
 const imageNuevosObject : GaleryImg[] = [
+  { imagen_main: Image_nuevos_10, title: 'Merry Grinchmas' },
+  { imagen_main: Image_nuevos_11, title: 'Jack MC' },
+  { imagen_main: Image_nuevos_12, title: 'Grinch Mode W' },
+  { imagen_main: Image_nuevos_13, title: 'Ho Ho Ho W' },
+  { imagen_main: Image_nuevos_14, title: 'Grinch mi dia W' },
+  { imagen_main: Image_nuevos_15, title: 'Grinch mi dia B' },
+  { imagen_main: Image_nuevos_16, title: 'Grinch MOde B' },
+  { imagen_main: Image_nuevos_17, title: 'Grinch Silueta' },
+  { imagen_main: Image_nuevos_18, title: 'Jack & Grinch' },
+  { imagen_main: Image_nuevos_19, title: 'Grinch' },
+  { imagen_main: Image_nuevos_20, title: 'Ansiedad Navidena' },
+  { imagen_main: Image_nuevos_21, title: 'Merry Grinchmas 2' },
   { imagen_main: Image_nuevos_1, title: 'McQueen hoodie' },
   { imagen_main: Image_nuevos_2, title: 'McQueen 1' },
   { imagen_main: Image_nuevos_3, title: 'McQueen 2' },
